68333b9c978b8bed7d9a13b2b0b13bbe3359f2d3
Xanixouille <49247581+Xanixouille@users.noreply.github.com>
2019-05-04 19:52:55 +0200

    Add files via upload

---
+++ Exemple_et_tests/Jason/Coordonnees.java
@@ -0,0 +1,31 @@
+public class Coordonnees{
+
+	private int x;
+	private int y;
+	private boolean touche; //Utilisé uniquement pour les objets Bateau
+
+	public Coordonnees(){
+		this.touche = false;
+	}
+
+	public void setXY(int x,int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setTouche(){
+		this.touche = true;
+	}
+
+	public int getX(){
+		return this.x;
+	}
+
+	public int getY(){
+		return this.y;
+	}
+
+	public boolean getTouche(){
+		return this.touche;
+	}
+}
